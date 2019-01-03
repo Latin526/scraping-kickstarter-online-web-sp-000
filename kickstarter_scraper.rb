84f619065eeb7ff7724f0bcc5b6896dc7ccdfeee
Yasmin Torres <yasmintorres90@hotmail.com>
2019-01-03 22:02:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,9 @@ require 'pry'
 
 def create_project_hash
   # write your code here
-  
+  binding.pry
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+ 
 end
